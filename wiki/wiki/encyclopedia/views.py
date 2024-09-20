57ff3f8c9c70fc6e0d1ef1abf774db6ebafcd1e8
satyavardhankoyalkar <145456047+satyavardhankoyalkar@users.noreply.github.com>
2024-09-20 12:13:15 +0530

    Add files via upload
    these are files of django project wiki encyclopedia

---
+++ wiki/wiki/encyclopedia/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render
+import markdown
+import random
+
+
+from . import util
+
+def covertm(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request,title):
+    html_content= covertm(title)
+    if html_content == None:
+        return render(request, "encyclopedia/err.html" , {
+            "message":"this entry does not exist"
+        })
+    else:
+        return render(request,"encyclopedia/entry.html",{
+            "title":title,
+            "content":html_content
+        })
+def search(request):
+        if request.method == "POST":
+             entry_search = request.POST['q']
+             html_content = covertm(entry_search)
+             if html_content is not None:
+                   return render(request,"encyclopedia/entry.html",{
+                        "title":entry_search,
+                        "content":html_content
+                    })
+             else:
+                  alle = util.list_entries()
+                  recom = []
+                  for entry in alle:
+                       if entry_search.lower() in entry.lower():
+                            recom.append(entry)
+                  return render(request,"encyclopedia/search.html",{
+                       "recom":recom,
+                       
+                  })   
+def newp(request):
+     if request.method == "GET":
+          return render(request, "encyclopedia/new.html")
+     else:
+          title = request.POST['title']
+          content = request.POST['content']
+          titleExist=util.get_entry(title)
+          if titleExist is not None:
+               return render(request,"encyclopedia/err.html",{"message": "Entry Exists Already"})
+          else:
+               util.save_entry(title, content)
+               html_content = covertm(title)
+               return render(request, "encyclopedia/entry.html", {
+                    "title": title,
+                    "content": html_content
+               })
+def edit(request):
+     if request.method == 'POST':
+          title = request.POST['entry_title']
+          content = util.get_entry(title)
+          return render(request, "encyclopedia/edit.html", {
+               "title": title,
+               "content": content
+          })
+def save_edit(request):
+     if request.method == "POST":
+          title = request.POST['title']
+          content = request.POST['content']
+          util.save_entry(title,content)
+          html_content = covertm(title)
+          return render(request, "encyclopedia/entry.html", {
+                    "title": title,
+                    "content": html_content
+          })
+def rand(request):
+     allEntries = util.list_entries()
+     rand_entry = random.choice(allEntries)
+     html_content = covertm(rand_entry)
+     return render(request,"encyclopedia/entry.html", {
+          "title": rand_entry,
+          "content": html_content
+     })     
